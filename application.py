06f1ef652c9c939c88672ac10a80a1ac2e8631be
Alejandro Gonzalez <alejandro@Alejandros-MacBook-Pro.local>
2019-10-14 16:56:46 -0700

    added name

--- application.py
+++ application.py
@@ -27,3 +27,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
